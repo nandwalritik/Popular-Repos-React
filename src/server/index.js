55135920d7e1e7f4ffa5567c841404467639ac91
Ritik Nandwal <48522685+nandwalritik@users.noreply.github.com>
2020-05-26 18:56:01 +0530

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -54,6 +54,6 @@ app.get("*", (req, res, next) => {
 
   })
 app.set( 'port', ( process.env.PORT || 5000 ));
-app.listen(app.get('port'), function() => {
+app.listen(app.get('port'), function(){
   console.log(`Server is listening on port: `+app.get('port'));
 })
